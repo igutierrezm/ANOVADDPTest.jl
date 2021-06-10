d74e91bb6b250a7ffa36377a8e63a2eee194d34a
Iván Gutiérrez <ivangutierrez1988@gmail.com>
2021-06-10 20:24:57 +0000

    Add a script explaining how to call fit() from R

---
+++ sandbox/R-example.R
@@ -0,0 +1,30 @@
+# El siguiente código funciona en julia 1.5.3 + R 4.0.4
+
+# Primero, instalemos y carguemos el paquete de R "JuliaConnectoR" 
+install.packages("JuliaConnectoR")
+library(JuliaConnectoR)
+
+# Verifiquemos si JuliaConnectoR detecta a julia
+JuliaConnectoR::juliaSetupOk() # debiese ser igual a TRUE
+
+# Ahora, instalemos los dos paquetes que hacen el trabajo en Julia
+juliaEval('Pkg.add(url = "https://github.com/igutierrezm/DPMNeal3.jl")')
+juliaEval('Pkg.add(url = "https://github.com/igutierrezm/ANOVADDPTest.jl")')
+
+# Una vez instalados, podemos crear wrappers usando la función juliaImport:
+ANOVADDPTest <- juliaImport("ANOVADDPTest")
+
+# La función es fit(), que retorna las cadenas para gamma.
+# Veamos un ejemplo:
+
+N <- 1000
+G <- 3
+y <- rnorm(N) # respuestas
+x <- as.integer(1 + 0:(N-1) %% G) # grupos
+res <- ANOVADDPTest$fit(y, x, iter = 2000L)
+
+# Una vez las cadenas, podemos traerlas a R y trabajarlas como es usual
+res %>%
+  juliaGet() %>%
+  purrr::reduce(rbind) %>%
+  colMeans()
