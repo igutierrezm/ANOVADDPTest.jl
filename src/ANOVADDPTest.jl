f2998ca12782bf728db187c7c836885840c824b1
Iván Gutiérrez <ivangutierrez1988@gmail.com>
2021-06-15 03:38:28 +0000

    Embrace the new interface disposed by DPMNeal3.jl

--- src/ANOVADDPTest.jl
+++ src/ANOVADDPTest.jl
@@ -1,12 +1,13 @@
 module ANOVADDPTest
 
-using Random: MersenneTwister
+using Random: AbstractRNG, MersenneTwister
 using Distributions: logpdf
 using SpecialFunctions
 using Parameters: @unpack
-using DPMNeal3: GenericBlock, update!
-import DPMNeal3: update!, update_hyperpars!, update_suffstats!, logpredlik
-export GenericBlock, SpecificBlock, Data, update!, fit
+using DPMNeal3
+import DPMNeal3: parent_dpm, logpredlik, update_hyperpars!, update_suffstats!
+# export GenericBlock, SpecificBlock, Data, update!, fit
+export NormalDDP, Data, dp_mass, n_clusters, cluster_sizes, cluster_capacity, cluster_labels, active_clusters, passive_clusters, update!
 
 struct Data
     x::Vector{Int}
@@ -14,12 +15,12 @@ struct Data
 end
 
 """
-    SpecificBlock(G; v0 = 2.0, r0 = 1.0, u0 = 0.0, s0 = 1.0)
+    ANOVADDP_SS_Normal_NIG(rng::AbstractRNG, N::Int, G::Int; K0 = 5, a0 = 2.0, b0 = 4, v0 = 2.0, r0 = 1.0, u0 = 0.0, s0 = 1.0)
 
 TODO
 """
-struct SpecificBlock
-    G::Int
+struct NormalDDP <: AbstractDPM
+    parent::DPM
     v0::Float64
     r0::Float64
     u0::Float64
@@ -30,68 +31,56 @@ struct SpecificBlock
     s1::Vector{Vector{Float64}}
     πγ::Vector{Float64}
     γ::Vector{Bool}
-    function SpecificBlock(G; v0 = 2.0, r0 = 1.0, u0 = 0.0, s0 = 1.0)
-        γ = ones(Bool, G)
+    G::Int
+    function NormalDDP(
+            rng::AbstractRNG, 
+            N::Int, 
+            G::Int; 
+            K0::Int = 5, 
+            a0::Float64 = 2.0,
+            b0::Float64 = 4.0,
+            v0::Float64 = 2.0, 
+            r0::Float64 = 1.0, 
+            u0::Float64 = 0.0, 
+            s0::Float64 = 1.0
+        )
+        parent = DPM(rng, N; K0, a0, b0)
         v1 = [v0 * ones(G)]
         r1 = [r0 * ones(G)]
         u1 = [u0 * ones(G)]
         s1 = [s0 * ones(G)]
         πγ = ones(G) / G
-        new(G, v0, r0, u0, s0, v1, r1, u1, s1, πγ, γ)
+        γ = ones(Bool, G)
+        new(parent, v0, r0, u0, s0, v1, r1, u1, s1, πγ, γ, G)
     end
 end
 
-function resize!(sb::SpecificBlock, n::Integer)
-    @unpack G, v1, r1, u1, s1, v0, r0, u0, s0 = sb
-    while length(v1) < n
-        push!(v1, v0 * ones(G))
-        push!(r1, r0 * ones(G))
-        push!(u1, u0 * ones(G))
-        push!(s1, s0 * ones(G))
-    end    
+function parent_dpm(m::NormalDDP)
+    m.parent
 end
 
-function update_γ!(rng, sb::SpecificBlock, gb::GenericBlock, data)
-    @unpack πγ, γ = sb
-    @unpack A = gb
-
-    # Resample γ[g], given the other γ's
-    for g = 2:length(γ)
-        # log-odds (numerator)
-        γ[g] = 1
-        update_suffstats!(sb, gb, data)
-        log_num = log(πγ[sum(γ)])
-        for k ∈ A, j ∈ (1, g)
-            log_num += logmglik(sb, j, k)
-        end
-
-        # log-odds (denominator)
-        γ[g] = 0
-        update_suffstats!(sb, gb, data)
-        log_den = log(πγ[sum(γ)])
-        for k ∈ A, j ∈ (1)
-            log_den += logmglik(sb, j, k)
-            # println(logmglik(sb, j, k))
-        end
-
-        # log-odds and new γ[g]
-        log_odds = log_num - log_den
-        γ[g] = rand(rng) <= 1 / (1 + exp(-log_odds))
-    end
+function add_cluster!(m::NormalDDP)
+    @unpack G, v0, r0, u0, s0, v1, r1, u1, s1 = m
+    push!(v1, v0 * ones(G))
+    push!(r1, r0 * ones(G))
+    push!(u1, u0 * ones(G))
+    push!(s1, s0 * ones(G))
 end
 
-function update_suffstats!(sb::SpecificBlock, gb::GenericBlock, data)
+function update_suffstats!(m::NormalDDP, data)
+    @unpack v0, r0, u0, s0, v1, r1, u1, s1, γ = m
     @unpack y, x = data
-    @unpack N, A, d, n = gb
-    @unpack v1, r1, u1, s1, v0, r0, u0, s0, γ = sb
-    length(v1) < length(n) && resize!(sb, length(n))
-    for k in A
+    d = cluster_labels(m)
+    while length(v1) < cluster_capacity(m)
+        add_cluster!(m)
+    end
+    for k in active_clusters(m)
         v1[k] .= v0
         r1[k] .= r0
         u1[k] .= u0
         s1[k] .= s0
     end
-    for i = 1:N
+    for i = 1:length(y)
         zi = iszero(γ[x[i]]) ? 1 : x[i]
         di = d[i]
         v1[di][zi] += 1
@@ -101,15 +90,12 @@ function update_suffstats!(sb::SpecificBlock, gb::GenericBlock, data)
     end
 end
 
-function update_hyperpars!(rng, sb::SpecificBlock, gb::GenericBlock, data)
-    update_γ!(rng, sb, gb, data)
-end
-
-function update_suffstats!(sb::SpecificBlock, gb::GenericBlock, data, i, k1, k2)
-    @unpack n = gb
+function update_suffstats!(m::NormalDDP, data, i::Int, k1::Int, k2::Int)
     @unpack y, x = data
-    @unpack v1, r1, u1, s1, γ = sb
-    length(v1) < length(n) && resize!(sb, length(n))
+    @unpack v1, r1, u1, s1, γ = m
+    while length(v1) < cluster_capacity(m)
+        add_cluster!(m)
+    end
     zi = iszero(γ[x[i]]) ? 1 : x[i]
 
     # Modify cluster/group di/k2
@@ -127,10 +113,43 @@ function update_suffstats!(sb::SpecificBlock, gb::GenericBlock, data, i, k1, k2)
     r1[k1][zi] -= 1
 end
 
-function logpredlik(sb::SpecificBlock, gb::GenericBlock, data, i, k)
-    @unpack v1, r1, u1, s1, γ = sb
+function update_γ!(rng::AbstractRNG, m::NormalDDP, data)
+    @unpack πγ, γ = m
+    A = active_clusters(m)
+
+    # Resample γ[g], given the other γ's
+    for g = 2:length(γ)
+        # log-odds (numerator)
+        γ[g] = 1
+        update_suffstats!(m, data)
+        log_num = log(πγ[sum(γ)])
+        for k ∈ A, j ∈ (1, g)
+            log_num += logmglik(m, j, k)
+        end
+
+        # log-odds (denominator)
+        γ[g] = 0
+        update_suffstats!(m, data)
+        log_den = log(πγ[sum(γ)])
+        for k ∈ A, j ∈ (1)
+            log_den += logmglik(m, j, k)
+            # println(logmglik(m, j, k))
+        end
+
+        # log-odds and new γ[g]
+        log_odds = log_num - log_den
+        γ[g] = rand(rng) <= 1 / (1 + exp(-log_odds))
+    end
+end
+
+function update_hyperpars!(rng::AbstractRNG, m::NormalDDP, data)
+    update_γ!(rng, m, data)
+end
+
+function logpredlik(m::NormalDDP, data, i::Int, k::Int)
+    @unpack v1, r1, u1, s1, γ = m
     @unpack y, x = data
-    @unpack d = gb
+    d = cluster_labels(m)
     yi = y[i]
     di = d[i]
     zi = iszero(γ[x[i]]) ? 1 : x[i]
@@ -169,8 +188,8 @@ function logpredlik(sb::SpecificBlock, gb::GenericBlock, data, i, k)
     end
 end
 
-function logmglik(sb::SpecificBlock, j, k)
-    @unpack v0, v1, r0, r1, s0, s1 = sb
+function logmglik(m::NormalDDP, j::Int, k::Int)
+    @unpack v0, v1, r0, r1, s0, s1 = m
     return(
         0.5v0 * log(s0) -
         0.5v1[k][j] * log(s1[k][j]) +
@@ -181,21 +200,21 @@ function logmglik(sb::SpecificBlock, j, k)
     )
 end
 
-function fit(y, x; seed = 0, iter = 2000, warmup = iter ÷ 2, thin = 1)
-    rng = seed == 0 ? MersenneTwister() : MersenneTwister()
-    N = length(y)
-    G = length(unique(x))
-    data = Data(x, y)
-    sb = SpecificBlock(G)
-    gb = GenericBlock(rng, N)
-    γs = [zeros(Bool, G) for _ = 1:(iter - warmup) ÷ thin]
-    for t in 1:iter
-        update!(rng, sb, gb, data)
-        if t > warmup
-            γs[(t - warmup) ÷ thin] = sb.γ[:]
-        end
-    end
-    return γs
-end
+# function fit(y, x; seed = 0, iter = 2000, warmup = iter ÷ 2, thin = 1)
+#     rng = seed == 0 ? MersenneTwister() : MersenneTwister()
+#     N = length(y)
+#     G = length(unique(x))
+#     data = Data(x, y)
+#     sb = SpecificBlock(G)
+#     gb = GenericBlock(rng, N)
+#     γs = [zeros(Bool, G) for _ = 1:(iter - warmup) ÷ thin]
+#     for t in 1:iter
+#         update!(rng, m, data)
+#         if t > warmup
+#             γs[(t - warmup) ÷ thin] = m.γ[:]
+#         end
+#     end
+#     return γs
+# end
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -5,75 +5,87 @@ using Statistics: mean, var
 using Random
 using Test
 
-@testset "SpecificBlock" begin
-    G = 4
-    sb = SpecificBlock(G; v0 = 2, r0 = 3, u0 = 3.0, s0 = 9.0)
-    @test sb.G  == 4
-    @test sb.v0 == 2
-    @test sb.r0 == 3
-    @test sb.u0 == 3.0
-    @test sb.s0 == 9.0
-    @test sb.v1 == [2 * ones(G)]
-    @test sb.r1 == [3 * ones(G)]
-    @test sb.u1 == [3 * ones(G)]
-    @test sb.s1 == [9 * ones(G)]
-    @test sb.γ  == ones(Bool, G)
+@testset "NormalDDP" begin
+    rng = MersenneTwister(1)
+    N, G, v0, r0, u0, s0 = 10, 4, 2.0, 3.0, 3.0, 9.0
+    m = NormalDDP(rng, N, G; v0, r0, u0, s0)
+    @test m.G  == 4
+    @test m.v0 == 2
+    @test m.r0 == 3
+    @test m.u0 == 3.0
+    @test m.s0 == 9.0
+    @test m.v1 == [2 * ones(G)]
+    @test m.r1 == [3 * ones(G)]
+    @test m.u1 == [3 * ones(G)]
+    @test m.s1 == [9 * ones(G)]
+    @test m.γ  == ones(Bool, G)
 end
 
-@testset "resize!" begin
-    G = 4
-    n = 2
-    sb = SpecificBlock(G; v0 = 2, r0 = 3, u0 = 3.0, s0 = 9.0)
-    ANOVADDPTest.resize!(sb, n)
-    @test length(sb.v1) == n
-    @test length(sb.r1) == n
-    @test length(sb.u1) == n
-    @test length(sb.s1) == n
+@testset "NormalDDP inherited accessors" begin
+    rng = MersenneTwister(1)
+    N, G, K0, a0, b0, v0, r0, u0, s0 = 10, 4, 5, 2.0, 4.0, 2.0, 3.0, 3.0, 9.0
+    m = NormalDDP(rng, N, G; K0, a0, b0, v0, r0, u0, s0)
+    @test dp_mass(m) > 0.0
+    @test dp_mass(m) < Inf
+    @test n_clusters(m) == K0
+    @test sum(cluster_sizes(m) .== 0) == 1
+    @test sum(cluster_sizes(m)) == N
+    @test active_clusters(m) == Set(1:K0)
+    @test passive_clusters(m) == Set(K0 + 1)
+    @test cluster_capacity(m) == K0 + 1
+end
+
+@testset "add_cluster!" begin
+    rng = MersenneTwister(1)
+    N, G, K0, a0, b0, v0, r0, u0, s0 = 10, 4, 5, 2.0, 4.0, 2.0, 3.0, 3.0, 9.0
+    m = NormalDDP(rng, N, G; K0, a0, b0, v0, r0, u0, s0)
+    ANOVADDPTest.add_cluster!(m)
+    @test length(m.v1) == 2
+    @test length(m.r1) == 2
+    @test length(m.u1) == 2
+    @test length(m.s1) == 2
 end
 
 @testset "update_suffstats! (1)" begin
     rng = MersenneTwister(1)
     data = Data([1], [1.0])
-    v0, r0, u0, s0 = 1.0, 1.0, 0.0, 1.0
-    sb = SpecificBlock(1; v0, r0, u0, s0)
-    gb = GenericBlock(rng, 1)
-    ANOVADDPTest.update_suffstats!(sb, gb, data)
-    @test sb.v1[1][1] ≈ 2.0
-    @test sb.v1[2][1] ≈ 1.0
-    @test sb.r1[1][1] ≈ 2.0
-    @test sb.r1[2][1] ≈ 1.0
-    @test sb.u1[1][1] ≈ 0.5
-    @test sb.u1[2][1] ≈ 0.0
-    @test sb.s1[1][1] ≈ 1.5
-    @test sb.s1[2][1] ≈ 1.0
+    N, G, K0, v0, r0, u0, s0 = 1, 1, 1, 1.0, 1.0, 0.0, 1.0
+    m = NormalDDP(rng, N, G; K0, v0, r0, u0, s0)
+    ANOVADDPTest.update_suffstats!(m, data)
+    @test m.v1[1][1] ≈ 2.0
+    @test m.v1[2][1] ≈ 1.0
+    @test m.r1[1][1] ≈ 2.0
+    @test m.r1[2][1] ≈ 1.0
+    @test m.u1[1][1] ≈ 0.5
+    @test m.u1[2][1] ≈ 0.0
+    @test m.s1[1][1] ≈ 1.5
+    @test m.s1[2][1] ≈ 1.0
 end
 
 @testset "update_suffstats! (2)" begin
     rng = MersenneTwister(1)
     data = Data([1], [1.0])
-    v0, r0, u0, s0 = 1.0, 1.0, 0.0, 1.0
-    sb = SpecificBlock(1; v0, r0, u0, s0)
-    gb = GenericBlock(rng, 1)
-    ANOVADDPTest.update_suffstats!(sb, gb, data)
-    ANOVADDPTest.update_suffstats!(sb, gb, data, 1, 1, 2)
-    @test sb.v1[1][1] ≈ 1.0
-    @test sb.v1[2][1] ≈ 2.0
-    @test sb.r1[1][1] ≈ 1.0
-    @test sb.r1[2][1] ≈ 2.0
-    @test sb.u1[1][1] ≈ 0.0
-    @test sb.u1[2][1] ≈ 0.5
-    @test sb.s1[1][1] ≈ 1.0
-    @test sb.s1[2][1] ≈ 1.5
+    N, G, K0, v0, r0, u0, s0 = 1, 1, 1, 1.0, 1.0, 0.0, 1.0
+    m = NormalDDP(rng, N, G; K0, v0, r0, u0, s0)
+    ANOVADDPTest.update_suffstats!(m, data)
+    ANOVADDPTest.update_suffstats!(m, data, 1, 1, 2)
+    @test m.v1[1][1] ≈ 1.0
+    @test m.v1[2][1] ≈ 2.0
+    @test m.r1[1][1] ≈ 1.0
+    @test m.r1[2][1] ≈ 2.0
+    @test m.u1[1][1] ≈ 0.0
+    @test m.u1[2][1] ≈ 0.5
+    @test m.s1[1][1] ≈ 1.0
+    @test m.s1[2][1] ≈ 1.5
 end
 
 @testset "logpredlik (empty clusters)" begin
     rng = MersenneTwister(1)
     data = Data([1], [1.0])
-    v0, r0, u0, s0 = 1.0, 1.0, 0.0, 1.0
-    sb = SpecificBlock(1; v0, r0, u0, s0)
-    gb = GenericBlock(rng, 1)
-    ANOVADDPTest.update_suffstats!(sb, gb, data)
-    @test ANOVADDPTest.logpredlik(sb, gb, data, 1, first(gb.P)) ≈ (
+    N, G, K0, v0, r0, u0, s0 = 1, 1, 1, 1.0, 1.0, 0.0, 1.0
+    m = NormalDDP(rng, N, G; K0, v0, r0, u0, s0)
+    ANOVADDPTest.update_suffstats!(m, data)
+    @test ANOVADDPTest.logpredlik(m, data, 1, first(passive_clusters(m))) ≈ (
         0.5 * 1.0 * log(1.0) -
         0.5 * 2.0 * log(1.5) +
         loggamma(2.0 / 2) -
@@ -86,16 +98,15 @@ end
 @testset "logpredlik (non-empty clusters)" begin
     rng = MersenneTwister(1)
     data = Data([1, 1], [1.0, 0.0])
-    v0, r0, u0, s0 = 1.0, 1.0, 0.0, 1.0
-    sb = SpecificBlock(1; v0, r0, u0, s0)
-    gb = GenericBlock(rng, 2)
-    ANOVADDPTest.update_suffstats!(sb, gb, data)
-    @test sb.v1[1][1] ≈ 3.0
-    @test sb.r1[1][1] ≈ 3.0
-    @test sb.u1[1][1] ≈ 1/3
-    @test sb.s1[1][1] ≈ 5/3
+    N, G, K0, v0, r0, u0, s0 = 2, 1, 1, 1.0, 1.0, 0.0, 1.0
+    m = NormalDDP(rng, N, G; K0, v0, r0, u0, s0)
+    ANOVADDPTest.update_suffstats!(m, data)
+    @test m.v1[1][1] ≈ 3.0
+    @test m.r1[1][1] ≈ 3.0
+    @test m.u1[1][1] ≈ 1/3
+    @test m.s1[1][1] ≈ 5/3
 
-    @test ANOVADDPTest.logpredlik(sb, gb, data, 2, 1) ≈ (
+    @test ANOVADDPTest.logpredlik(m, data, 2, 1) ≈ (
         0.5 * 2 * log(1.5) -
         0.5 * 3 * log(5/3) +
         loggamma(3/2) -
@@ -108,110 +119,109 @@ end
 @testset "update! (1)" begin
     rng = MersenneTwister(1)
     data = Data([1, 1], [1.0, 0.0])
-    v0, r0, u0, s0 = 1.0, 1.0, 0.0, 1.0
-    sb = SpecificBlock(1; v0, r0, u0, s0)
-    gb = GenericBlock(rng, 2)
-    update!(rng, sb, gb, data)
+    N, G, K0, v0, r0, u0, s0 = 2, 1, 1, 1.0, 1.0, 0.0, 1.0
+    m = NormalDDP(rng, N, G; K0, v0, r0, u0, s0)
+    update!(rng, m, data)
 end
 
-@testset "update! (2)" begin
-    rng = MersenneTwister(1)
-    N, F = 1000, 3
-    y = randn(rng, N)
-    x = [rand(rng, 1:3, F) for _ in 1:N]
-    x = denserank(x)
-    G = length(unique(x))
-    data = Data(x, y)
-    sb = SpecificBlock(G)
-    gb = GenericBlock(rng, N)
-    update!(rng, sb, gb, data)
-end
+# @testset "update! (2)" begin
+#     rng = MersenneTwister(1)
+#     N, F = 1000, 3
+#     y = randn(rng, N)
+#     x = [rand(rng, 1:3, F) for _ in 1:N]
+#     x = denserank(x)
+#     G = length(unique(x))
+#     data = Data(x, y)
+#     sb = SpecificBlock(G)
+#     gb = GenericBlock(rng, N)
+#     update!(rng, sb, gb, data)
+# end
 
-@testset "final_example" begin
-    rng = MersenneTwister(1)
-    N, F = 1000, 1
-    y = randn(rng, N)
-    x = [rand(rng, 1:3, F) for _ in 1:N]
-    x = denserank(x)
-    for i = 1:N
-        if x[i] == 2
-            y[i] += 10.0
-        end
-    end
-    y .= (y .- mean(y)) ./ √var(y)
-    G = length(unique(x))
-    data = Data(x, y)
-    sb = SpecificBlock(G)
-    gb = GenericBlock(rng, N; K0 = 1)
-    for t in 1:10
-        update!(rng, sb, gb, data)
-        println(sb.γ[:])
-    end
-end
+# @testset "final_example" begin
+#     rng = MersenneTwister(1)
+#     N, F = 1000, 1
+#     y = randn(rng, N)
+#     x = [rand(rng, 1:3, F) for _ in 1:N]
+#     x = denserank(x)
+#     for i = 1:N
+#         if x[i] == 2
+#             y[i] += 10.0
+#         end
+#     end
+#     y .= (y .- mean(y)) ./ √var(y)
+#     G = length(unique(x))
+#     data = Data(x, y)
+#     sb = SpecificBlock(G)
+#     gb = GenericBlock(rng, N; K0 = 1)
+#     for t in 1:10
+#         update!(rng, sb, gb, data)
+#         println(sb.γ[:])
+#     end
+# end
 
-@testset "fit (1)" begin
-    rng = MersenneTwister(1)
-    N, F = 1000, 1
-    y = randn(rng, N)
-    x = [rand(rng, 1:3, F) for _ in 1:N]
-    x = denserank(x)
-    for i = 1:N
-        if x[i] == 2
-            y[i] += 10.0
-        end
-    end
-    y .= (y .- mean(y)) ./ √var(y)
-    γb = mean(fit(y, x; seed = 1))
-    @test γb[1] ≈ 1.0
-    @test γb[2] ≥ 0.85
-    @test γb[3] ≤ 0.15
-end
+# @testset "fit (1)" begin
+#     rng = MersenneTwister(1)
+#     N, F = 1000, 1
+#     y = randn(rng, N)
+#     x = [rand(rng, 1:3, F) for _ in 1:N]
+#     x = denserank(x)
+#     for i = 1:N
+#         if x[i] == 2
+#             y[i] += 10.0
+#         end
+#     end
+#     y .= (y .- mean(y)) ./ √var(y)
+#     γb = mean(fit(y, x; seed = 1))
+#     @test γb[1] ≈ 1.0
+#     @test γb[2] ≥ 0.85
+#     @test γb[3] ≤ 0.15
+# end
 
-@testset "fit (2)" begin
-    rng = MersenneTwister(1)
-    N, F = 1000, 1
-    y = randn(rng, N)
-    x = [rand(rng, 1:3, F) for _ in 1:N]
-    x = denserank(x)
-    for i = 1:N
-        if x[i] == 3
-            y[i] += 10.0
-        end
-    end
-    y .= (y .- mean(y)) ./ √var(y)
-    γb = mean(fit(y, x; seed = 1))
-    @test γb[1] ≈ 1.0
-    @test γb[2] ≤ 0.15
-    @test γb[3] ≥ 0.85
-end
+# @testset "fit (2)" begin
+#     rng = MersenneTwister(1)
+#     N, F = 1000, 1
+#     y = randn(rng, N)
+#     x = [rand(rng, 1:3, F) for _ in 1:N]
+#     x = denserank(x)
+#     for i = 1:N
+#         if x[i] == 3
+#             y[i] += 10.0
+#         end
+#     end
+#     y .= (y .- mean(y)) ./ √var(y)
+#     γb = mean(fit(y, x; seed = 1))
+#     @test γb[1] ≈ 1.0
+#     @test γb[2] ≤ 0.15
+#     @test γb[3] ≥ 0.85
+# end
 
-@testset "fit (3)" begin
-    rng = MersenneTwister(1)
-    N, F = 1000, 1
-    y = randn(rng, N)
-    x = [rand(rng, 1:3, F) for _ in 1:N]
-    x = denserank(x)
-    for i = 1:N
-        if x[i] != 1
-            y[i] += 10.0
-        end
-    end
-    y .= (y .- mean(y)) ./ √var(y)
-    γb = mean(fit(y, x; seed = 1))
-    @test γb[1] ≈ 1.0
-    @test γb[2] ≥ 0.85
-    @test γb[3] ≥ 0.85
-end
+# @testset "fit (3)" begin
+#     rng = MersenneTwister(1)
+#     N, F = 1000, 1
+#     y = randn(rng, N)
+#     x = [rand(rng, 1:3, F) for _ in 1:N]
+#     x = denserank(x)
+#     for i = 1:N
+#         if x[i] != 1
+#             y[i] += 10.0
+#         end
+#     end
+#     y .= (y .- mean(y)) ./ √var(y)
+#     γb = mean(fit(y, x; seed = 1))
+#     @test γb[1] ≈ 1.0
+#     @test γb[2] ≥ 0.85
+#     @test γb[3] ≥ 0.85
+# end
 
-@testset "fit (4)" begin
-    rng = MersenneTwister(1)
-    N, F = 1000, 1
-    y = randn(rng, N)
-    x = [rand(rng, 1:3, F) for _ in 1:N]
-    x = denserank(x)
-    y .= (y .- mean(y)) ./ √var(y)
-    γb = mean(fit(y, x; seed = 1))
-    @test γb[1] ≈ 1.0
-    @test γb[2] ≤ 0.15
-    @test γb[3] ≤ 0.15
-end
+# @testset "fit (4)" begin
+#     rng = MersenneTwister(1)
+#     N, F = 1000, 1
+#     y = randn(rng, N)
+#     x = [rand(rng, 1:3, F) for _ in 1:N]
+#     x = denserank(x)
+#     y .= (y .- mean(y)) ./ √var(y)
+#     γb = mean(fit(y, x; seed = 1))
+#     @test γb[1] ≈ 1.0
+#     @test γb[2] ≤ 0.15
+#     @test γb[3] ≤ 0.15
+# end
