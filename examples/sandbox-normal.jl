14f958f7d5a8d652d0edb3b910f8d5740c7a1ceb
Iván Gutiérrez <ivangutierrez1988@gmail.com>
2021-07-28 05:01:16 +0000

    Add specific functions for simple and interaction effects probabilities

--- examples/sandbox-normal.jl
+++ examples/sandbox-normal.jl
@@ -159,6 +159,10 @@ predict =
     x -> NormalData(X = [x[1] x[2]], y = x[3])
 chain = train(rng, m, data, predict)
 
+simple_effect_probabilities(chain, predict)
+interaction_effect_probabilities(chain, predict)
+
+
 df = DataFrame(
     x = predict.x,
     y = predict.y,
@@ -173,52 +177,3 @@ df = DataFrame(
     markerstrokealpha = 0.9,
     bg = RGB(0.2, 0.2, 0.2)
 )
-
-function foo(chain, predict)
-    Ngroups = length(predict.Xunique)
-    Nvars = size(predict.X, 2)
-    gamma = hcat(expandgrid([1], [[0, 1] for _ in 1:Ngroups-1]...)...)
-    df = DataFrame(:gamma => [gamma[i, :] for i in 1:size(gamma, 1)])
-    df1 = deepcopy(df)
-    for var in 1:Nvars
-        df1[!, "var_$var"] = zeros(Bool, size(df1, 1))
-        for h in 1:size(df1, 1)
-            for row in 1:Ngroups
-                predict.Xunique[row][var] == 1 && continue
-                df1[h, "gamma"][row] == 0 && continue
-                df1[h, "var_$var"] = true
-            end
-        end
-    end
-
-    for var1 in 1:Nvars, var2 in 1:Nvars
-        df1[!, "var_$(var1)_$(var2)"] = zeros(Bool, size(df1, 1))
-        for h in 1:size(df1, 1)
-            for row in 1:Ngroups
-                predict.Xunique[row][var1] == 1 && continue
-                predict.Xunique[row][var2] == 1 && continue
-                df1[h, "gamma"][row] == 0 && continue
-                df1[h, "var_$(var1)_$(var2)"] = true
-            end
-        end
-    end
-
-    unique_gamma = unique(chain.gamma)
-    Ngammas = length(unique_gamma)
-    Niter = length(chain.gamma)
-    gamma_prob = zeros(Ngammas)
-    for j in 1:Ngammas
-        gamma_prob[j] = sum([chain.gamma[i] == unique_gamma[j] for i in 1:Niter])
-    end
-    gamma_prob /= sum(gamma_prob)
-    probs = DataFrame(:gamma => unique_gamma, :prob => gamma_prob)
-    df2 = leftjoin(df1, probs, on = :gamma)
-    df2[!, :prob] = coalesce.(df2[!, :prob], 0)
-    return df2
-end
-probs = foo(chain, predict)
-
-# Ejemplo de uso,
-sum(probs[!, :prob] .* probs[!, :var_1])
-sum(probs[!, :prob] .* probs[!, :var_2])
-sum(probs[!, :prob] .* probs[!, :var_1_1])
--- src/ANOVADDPTest.jl
+++ src/ANOVADDPTest.jl
@@ -5,10 +5,11 @@ using Distributions: logpdf, NegativeBinomial
 using SpecialFunctions: loggamma, logbeta, logfactorial
 using ExtractMacro: @extract
 using DPMNeal3
+using DataFrames
 import Base.length
 import DPMNeal3: parent_dpm, logpredlik, update_hyperpars!, update_suffstats!
 # export GenericBlock, SpecificBlock, Data, update!, train
-export NormalDDP, PoissonDDP, NormalData, PoissonData, BernoulliDDP, BernoulliData, dp_mass, n_clusters, cluster_sizes, cluster_capacity, cluster_labels, active_clusters, passive_clusters, update!, train
+export NormalDDP, PoissonDDP, NormalData, PoissonData, BernoulliDDP, BernoulliData, dp_mass, n_clusters, cluster_sizes, cluster_capacity, cluster_labels, active_clusters, passive_clusters, update!, train, simple_effect_probabilities, interaction_effect_probabilities
 using StatsBase: counts, denserank
 
 include("normal.jl")
@@ -48,6 +49,91 @@ function train(rng, m::AbstractDPM, train, predict; iter = 2000, warmup = iter Ã
         end
     end
     return DPM_MCMC_Chain(gammachain, fchain)
+end
+
+function simple_effect_probabilities(chain, predict)
+    Ngroups = length(predict.Xunique)
+    Nvars = size(predict.X, 2)
+    gamma = hcat(expandgrid([1], [[0, 1] for _ in 1:Ngroups-1]...)...)
+    df = DataFrame(:gamma => [gamma[i, :] for i in 1:size(gamma, 1)])
+    df1 = deepcopy(df)
+    for var in 1:Nvars
+        df1[!, "var_$var"] = zeros(Bool, size(df1, 1))
+        for h in 1:size(df1, 1)
+            for row in 1:Ngroups
+                predict.Xunique[row][var] == 1 && continue
+                df1[h, "gamma"][row] == 0 && continue
+                df1[h, "var_$var"] = true
+            end
+        end
+    end
+
+    unique_gamma = unique(chain.gamma)
+    Ngammas = length(unique_gamma)
+    Niter = length(chain.gamma)
+    gamma_prob = zeros(Ngammas)
+    for j in 1:Ngammas
+        gamma_prob[j] = sum([chain.gamma[i] == unique_gamma[j] for i in 1:Niter])
+    end
+    gamma_prob /= sum(gamma_prob)
+    probs = DataFrame(:gamma => unique_gamma, :prob => gamma_prob)
+    df2 = leftjoin(df1, probs, on = :gamma)
+    df2[!, :prob] = coalesce.(df2[!, :prob], 0)
+
+    df3 = df2[!, Not([:gamma, :prob])]
+    df4 = combine(df3, names(df3) .=> x -> sum(x .* df2.prob), renamecols = false)
+    df5 = stack(df4)
+    return df5
+end
+
+function interaction_effect_probabilities(chain, predict)
+    Ngroups = length(predict.Xunique)
+    Nvars = size(predict.X, 2)
+    gamma = hcat(expandgrid([1], [[0, 1] for _ in 1:Ngroups-1]...)...)
+    df = DataFrame(:gamma => [gamma[i, :] for i in 1:size(gamma, 1)])
+    df1 = deepcopy(df)
+    for var1 in 1:Nvars, var2 in 1:Nvars
+        var1 â‰¥ var2 && continue
+        df1[!, "var_$(var1)_$(var2)"] = zeros(Bool, size(df1, 1))
+        for h in 1:size(df1, 1)
+            for row in 1:Ngroups
+                predict.Xunique[row][var1] == 1 && continue
+                predict.Xunique[row][var2] == 1 && continue
+                df1[h, "gamma"][row] == 0 && continue
+                df1[h, "var_$(var1)_$(var2)"] = true
+            end
+        end
+    end
+
+    unique_gamma = unique(chain.gamma)
+    Ngammas = length(unique_gamma)
+    Niter = length(chain.gamma)
+    gamma_prob = zeros(Ngammas)
+    for j in 1:Ngammas
+        gamma_prob[j] = sum([chain.gamma[i] == unique_gamma[j] for i in 1:Niter])
+    end
+    gamma_prob /= sum(gamma_prob)
+    probs = DataFrame(:gamma => unique_gamma, :prob => gamma_prob)
+    df2 = leftjoin(df1, probs, on = :gamma)
+    df2[!, :prob] = coalesce.(df2[!, :prob], 0)
+
+    df3 = df2[!, Not([:gamma, :prob])]
+    df4 = combine(df3, names(df3) .=> x -> sum(x .* df2.prob), renamecols = false)
+    df5 = stack(df4)
+    return df5
+end
+
+function expandgrid(levels...)
+    lengths = length.(levels)
+    inner = 1
+    outer = prod(lengths)
+    grid = []
+    for i in 1:length(levels)
+        outer = div(outer, lengths[i])
+        push!(grid, repeat(levels[i], inner=inner, outer=outer))
+        inner *= lengths[i]
+    end
+    Tuple(grid)
 end
 
 end # module
