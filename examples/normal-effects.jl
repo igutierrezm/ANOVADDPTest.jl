8a3fa7a7567442c6902fc397e157b2bf25704f39
Iván Gutiérrez <ivangutierrez1988@gmail.com>
2021-08-25 05:07:23 +0000

    Rename the examples

--- examples/normal-effects.jl
+++ examples/normal-effects.jl
@@ -5,20 +5,26 @@ using DataFrames
 using Distributions
 using Random
 
-rng = MersenneTwister(1);
-N = 1000;
-D = 2
+# Here, I simulate data
+rng = MersenneTwister(1); # seed
+N = 1000; # number of observations
+D = 2; # number of covariates
 X = rand(rng, 1:2, N, D);
-y = randn(rng, N);
-for i in 1:N
-    if X[i, 1] == 2 && X[i, 2] == 1
-        y[i] += 1
-    end
-end
+y = randn(rng, N); # clearly, all groups are equal
+
+# Organize the data
 data = NormalData(X, y);
+
+# Create some data for prediction (useful for density plots)
 pred = expandgrid([1:2 for _ in 1:D]...,  range(-2.0, stop = 2.0, length = 50));
 pred = NormalData(hcat(pred[1:D]...), pred[D+1]);
+
+# Create the model
 m = NormalDDP(rng, N, length(data.Xunique));
+
+# Fit the model
 ch = train(rng, m, data, pred);
+
+# Compute the effects
 simple_effect_probabilities(ch, data)
 interaction_effect_probabilities(ch, data)
