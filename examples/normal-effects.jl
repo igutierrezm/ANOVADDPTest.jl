dc7494c9c3636b4ebae02b79e9f861dd07f4b8af
Iván Gutiérrez <ivangutierrez1988@gmail.com>
2021-08-31 21:31:12 +0000

    Fix a bug in simple_effect_probabilities and interaction_effect_probabilities

--- examples/normal-effects.jl
+++ examples/normal-effects.jl
@@ -9,14 +9,17 @@ using Random
 rng = MersenneTwister(1); # seed
 N = 1000; # number of observations
 D = 2; # number of covariates
-X = rand(rng, 1:2, N, D);
+X = rand(rng, 1:3, N, D);
 y = randn(rng, N); # clearly, all groups are equal
+for i in 1:N
+    ((X[i, 1] != 1) && (X[i, 2] == 1)) && (y[i] += 1.0)
+end
 
 # Organize the data
 data = NormalData(X, y);
 
 # Create some data for prediction (useful for density plots)
-pred = expandgrid([1:2 for _ in 1:D]...,  range(-2.0, stop = 2.0, length = 50));
+pred = expandgrid([1:3 for _ in 1:D]...,  range(-2.0, stop = 2.0, length = 50));
 pred = NormalData(hcat(pred[1:D]...), pred[D+1]);
 
 # Create the model
--- src/simple_effect_probabilities.jl
+++ src/simple_effect_probabilities.jl
@@ -4,8 +4,10 @@ function simple_effect_probabilities(chain, data)
     nvar = size(γpost_x, 2)
     p_effects = zeros(nvar)
     for i in 1:nvar
-        subset = γpost_x[:, i] .!= γpost_x[1, i]
-        p_effects[i] = mean(minimum(ch_gamma_mat[:, subset], dims = 2))
+        subset = deepcopy(γpost_x)
+        subset[:, i] = 2 .- (subset[:, i] .!= 1)
+        subset = maximum(subset, dims = 2)[:, 1] .== 1
+        p_effects[i] = mean(maximum(ch_gamma_mat[:, subset], dims = 2))
     end
     return DataFrame(var1 = 1:nvar, prob = p_effects)
 end
