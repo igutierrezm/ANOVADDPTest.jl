06dea11df0ce9470e25823c5e7f1c8504621c220
Iván Gutiérrez <ivangutierrez1988@gmail.com>
2021-09-15 02:54:37 +0000

    Clean the script

--- examples/r-example.R
+++ examples/r-example.R
@@ -1,7 +1,9 @@
-install.packages("JuliaConnectoR")
 install.packages("dplyr")
-library(JuliaConnectoR)
+install.packages("ggplot2")
+install.packages("JuliaConnectoR")
 library(dplyr)
+library(ggplot2)
+library(JuliaConnectoR)
 juliaSetupOk()
 
 # Install ANOVADDPTest.jl
@@ -29,13 +31,19 @@ data <- simulate_sample_normal(1, 1000)
 
 # Fit the model
 fit <- ANOVADDPTest$anova_bnp_normal(data$y, data$X)
-ANOVADDPTest$group_probs(fit)
 
+# Retrieve the posterior probability of each group
+group_probs <- ANOVADDPTest$group_probs(fit) %>% as.data.frame()
+group_probs
 
-juliaEval('
-    Pkg.add("DataFrames");
-    using DataFrames;
-    a = DataFrame([[1]], ["group1"]);
-')
+# Retrieve the meaning of each group
+group_codes <- ANOVADDPTest$group_codes(fit) %>% as.data.frame()
+group_codes
 
-juliaGet(a)
+# Retrieve the posterior predictive density
+fpost <- ANOVADDPTest$fpost(fit) %>% as.data.frame()
+fpost
+
+# Plot the posterior predictive density
+p <- ggplot(fpost, aes(x = y, y = f, color = group)) + geom_line()
+ggsave("fig1.png", p)
