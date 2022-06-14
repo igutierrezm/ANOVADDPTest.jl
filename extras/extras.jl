c5c0afacc05ddc423a9061295fe96a80eca8d058
Iván Gutiérrez <ivangutierrez1988@gmail.com>
2022-06-14 23:27:18 +0000

    Allow to standardize the responses in the normal model

--- extras/extras.jl
+++ extras/extras.jl
@@ -1,19 +1,19 @@
 using Revise
-
 using ANOVADDPTest
-using ANOVADDPTest: dp_mass, n_clusters,  cluster_sizes
-using ANOVADDPTest: active_clusters, passive_clusters, cluster_capacity, update!
+using DataFrames
 using Distributions
-using SpecialFunctions
-using StatsBase: mode
-using Statistics: mean, var
-using Random
-using Test
-using CSV
-using Tables
+using Statistics
+using Random 
+using AlgebraOfGraphics, CairoMakie
 
-X = CSV.File("Xmat.csv") |> Tables.matrix;
-y = CSV.File("yvec.csv") |> Tables.matrix;
-y = y[:];
-ch, data0 = anova_bnp_normal(y, X);
-simple_effect_probabilities(ch, data0)
+Random.seed!(1)
+X = ones(Int, 1000, 1)
+y = 1 .+ 2 * randn(1000)
+
+fitted_model = anova_bnp_normal(y, X; standardize_y = true);
+f = fpost(fitted_model)
+f.ftrue = pdf.(Normal(1, 2), f.y)
+f = stack(f, [:f, :ftrue])
+
+p = data(f) * mapping(:y, :value) * visual(Lines) * mapping(color = :variable)
+draw(p)
