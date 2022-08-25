dc4f3810641ea835990ff0b1ecc85fdd7a7894b2
Iván Gutiérrez <ivangutierrez1988@gmail.com>
2022-08-25 12:20:19 -0400

    Add a berpoi model

---
+++ src/anova_bnp_berpoi.jl
@@ -0,0 +1,84 @@
+begin
+    using Revise
+    using ANOVADDPTest
+    using StatsModels
+    using Statistics
+    using DataFrames
+    using Distributions
+    using Random
+end
+
+# Simulate sample
+function simulate_sample_berpoi(rng, N)
+    X = rand(rng, 1:2, N, 2);
+    λ = 1.0
+    y = rand(rng, Poisson(λ), N);
+    for i in 1:N
+        ((X[i, 1] == 2) && (X[i, 2] == 2)) && (y[i] += 1)
+    end
+    return y, X
+end
+
+# Fit the model in a more pleasant way
+function anova_bnp_berpoi(
+    y::Vector{Int},
+    X::Matrix{Int};
+    iter::Int = 4000, # taken from rstan::stan()
+    warmup::Int = 2000, # taken from rstan::stan()
+    seed::Int = 1, # taken from rstan::stan()
+    rho::Float64 = 1.0,
+    a::Float64 = 2.0,
+    b::Float64 = 4.0,
+    a1::Float64 = 2.0,
+    b1::Float64 = 4.0,
+    alpha0::Float64 = 1.0,
+    beta0::Float64 = 1.0,
+    lb::Int = minimum(y),
+    ub::Int = maximum(y)
+)
+    # Set data for training
+    data0 = BerPoiData(X, y)
+
+    # Set data for prediction
+    ngroups = length(data0.Xunique)
+    y1 = lb:ub |> x -> repeat(x, inner = ngroups)
+    X1 = repeat(vcat(data0.Xunique'...), ub - lb + 1)
+    data1 = BerPoiData(X1, y1)
+
+    # Initialize the model
+    N = length(y)
+    rng = MersenneTwister(seed)
+    model = BerPoiDDP(rng, N, ngroups; a, b, a1, b1, alpha0, beta0, rho)
+
+    # Train the model
+    ch = train(rng, model, data0, data1; iter, warmup);
+
+    # Compute p(gamma | y)
+    group_probs = gamma_posterior(ch);
+
+    # Compute the codebook
+    group_codes = gamma_codebook(data0);
+
+    # Compute the effects
+    effects1 = simple_effect_probabilities(ch, data0)
+    effects2 = interaction_effect_probabilities(ch, data0)
+
+    # Compute p(y0 | y)
+    fpost = DataFrame(group = data1.x, y = data1.y, f = mean(ch.f))
+
+    return anova_bnp_fitted(
+        group_codes,
+        group_probs,
+        effects1,
+        effects2,
+        fpost
+    )
+end
+
+# # Example
+# N = 1000;
+# rng = MersenneTwister(1);
+# y, X = simulate_sample_berpoi(rng, N);
+# data1 = anova_bnp_berpoi(y, X)
+# effects1(data1)
+# effects2(data1)
--- src/berpoi.jl
+++ src/berpoi.jl
@@ -14,42 +14,50 @@ BerPoiData(x::Vector{Int}, y::Vector{Int}) = BerPoiData(x[:, :], y)
 
 struct BerPoiDDP <: AbstractDPM
     parent::DPM
-    a0ϵ::Float64
-    b0ϵ::Float64
-    a0λ::Float64
-    b0λ::Float64
-    a1ϵ::Vector{Vector{Float64}}
-    b1ϵ::Vector{Vector{Float64}}
-    a1λ::Vector{Vector{Float64}}
-    b1λ::Vector{Vector{Float64}}
-    sumlogfactyu::Vector{Vector{Float64}}
-    u::Vector{Bool}
+    a1::Float64 # λ ~ Gamma(a1, b1) (λ from the berpoi distribution)
+    b1::Float64 # λ ~ Gamma(a1, b1) (λ from the berpoi distribution)
+    alpha0::Float64 # α ~ Beta(α0, β0) (α from the berpoi distribution)
+    beta0::Float64 # α ~ Beta(α0, β0) (α from the berpoi distribution)
+    a1_post::Vector{Vector{Int}}
+    b1_post::Vector{Vector{Int}}
+    alpha0_post::Vector{Vector{Float64}}
+    beta0_post::Vector{Vector{Float64}}
+    sumlogfactystar::Vector{Vector{Float64}}
     gammaprior::Womack
     gamma::Vector{Bool}
     ngroups::Int
-    function PoissonDDP(
+    zberpoi::Vector{Bool}
+    alphaberpoi::Vector{Vector{Float64}}
+    lambdaberpoi::Vector{Vector{Float64}}
+    function BerPoiDDP(
         rng::AbstractRNG,
         N::Int,
         ngroups::Int;
         K0::Int = 1,
-        a0α::Float64 = 2.0,
-        b0α::Float64 = 4.0,
-        a0ϵ::Float64 = 2.0,
-        b0ϵ::Float64 = 4.0,
-        a0λ::Float64 = 2.0,
-        b0λ::Float64 = 4.0,
+        a::Float64 = 2.0,
+        b::Float64 = 4.0,
+        alpha0::Float64 = 1.0,
+        beta0::Float64 = 1.0,
+        a1::Float64 = 2.0,
+        b1::Float64 = 4.0,
         rho::Float64 = 1.0,
     )
-        parent = DPM(rng, N; K0, a0 = a0α, b0 = b0α)
-        a1ϵ = [a0ϵ * ones(Int, ngroups)]
-        b1ϵ = [b0ϵ * ones(Int, ngroups)]
-        a1λ = [a0λ * ones(Int, ngroups)]
-        b1λ = [b0λ * ones(Int, ngroups)]
-        sumlogfactyu = [zeros(ngroups)]
-        u = zeros(N)
+        parent = DPM(rng, N; K0, a0 = a, b0 = b)
+        a1_post = [a1 * ones(Int, ngroups)]
+        b1_post = [b1 * ones(Int, ngroups)]
+        alpha0_post = [alpha0 * ones(Int, ngroups)]
+        beta0_post = [beta0 * ones(Int, ngroups)]
+        sumlogfactystar = [zeros(ngroups)]
         gammaprior = Womack(ngroups - 1, rho)
         gamma = ones(Bool, ngroups)
-        new(parent, a0ϵ, b0ϵ, a0λ, b0λ, a1ϵ, b1ϵ, a1λ, b1λ, sumlogfactyu, u, gammaprior, gamma, ngroups)
+        zberpoi = zeros(Bool, N)
+        alphaberpoi = [0.5 * ones(Int, ngroups)]
+        lambdaberpoi = [ones(Int, ngroups)]
+        new(
+            parent, a1, b1, alpha0, beta0, a1_post, b1_post,
+            alpha0_post, beta0_post, sumlogfactystar,
+            gammaprior, gamma, ngroups, zberpoi, alphaberpoi, lambdaberpoi
+        )
     end
 end
 
@@ -58,136 +66,101 @@ function parent_dpm(model::BerPoiDDP)
 end
 
 function add_cluster!(model::BerPoiDDP)
-    @extract model : ngroups a0ϵ b0ϵ a1ϵ b1ϵ a0λ b0λ a1λ b1λ sumlogfactyu
-    push!(a1ϵ, a0ϵ * ones(ngroups))
-    push!(b1ϵ, b0ϵ * ones(ngroups))
-    push!(a1λ, a0λ * ones(ngroups))
-    push!(b1λ, b0λ * ones(ngroups))
-    push!(sumlogfactyu, zeros(ngroups))
+    @extract model : ngroups a1 b1 alpha0 beta0
+    @extract model : a1_post b1_post alpha0_post beta0_post
+    @extract model : sumlogfactystar alphaberpoi lambdaberpoi
+    push!(a1_post, a1 * ones(ngroups))
+    push!(b1_post, b1 * ones(ngroups))
+    push!(alpha0_post, alpha0 * ones(ngroups))
+    push!(beta0_post, beta0 * ones(ngroups))
+    push!(alphaberpoi, 0.5 * ones(ngroups))
+    push!(lambdaberpoi, ones(ngroups))
+    push!(sumlogfactystar, zeros(ngroups))
 end
 
 function update_suffstats!(model::BerPoiDDP, data)
     @extract data : y x
-    @extract model : a0ϵ b0ϵ a1ϵ b1ϵ a0λ b0λ a1λ b1λ sumlogfactyu u gamma
+    @extract model : a1 b1 alpha0 beta0
+    @extract model : a1_post b1_post alpha0_post beta0_post sumlogfactystar gamma zberpoi
     d = cluster_labels(model)
     while length(a1_post) < cluster_capacity(model)
         add_cluster!(model)
     end
     for k in active_clusters(model)
-        a1ϵ[k] .= a0ϵ
-        b1ϵ[k] .= b0ϵ
-        a1λ[k] .= a0λ
-        b1λ[k] .= b0λ
-        sumlogfactyu[k] .= 0.0
+        a1_post[k] .= a1
+        b1_post[k] .= b1
+        alpha0_post[k] .= alpha0
+        beta0_post[k] .= beta0
+        sumlogfactystar[k] .= 0.0
     end
-    for i = 1:length(y)
+    for i in eachindex(y)
         di = d[i]
         zi = iszero(gamma[x[i]]) ? 1 : x[i]
-        a1λ[di][zi] += y[i] - u[i]
-        b1λ[di][zi] += 1
-        a1ϵ[di][zi] += u[i]
-        b1ϵ[di][zi] += 1 - u[i]
-        sumlogfactyu[di][zi] += logfactorial(y[i] - u[i])
+        sumlogfactystar[di][zi] += logfactorial(y[i] - zberpoi[i])
+        a1_post[di][zi] += y[i]
+        b1_post[di][zi] += 1
+        alpha0_post[di][zi] += zberpoi[i]
+        beta0_post[di][zi] += 1 - zberpoi[i]
     end
 end
 
 function update_suffstats!(model::BerPoiDDP, data, i::Int, k1::Int, k2::Int)
     @extract data : y x
-    @extract model : a0ϵ b0ϵ a1ϵ b1ϵ a0λ b0λ a1λ b1λ sumlogfactyu u gamma
-    while length(a1λ) < cluster_capacity(model)
+    @extract model : a1_post b1_post alpha0_post beta0_post
+    @extract model : sumlogfactystar gamma zberpoi
+    while length(a1_post) < cluster_capacity(model)
         add_cluster!(model)
     end
     zi = iszero(gamma[x[i]]) ? 1 : x[i]
 
     # Modify cluster/group k2/zi
-    a1λ[k2][zi] += y[i] - u[i]
-    b1λ[k2][zi] += 1
-    a1ϵ[k2][zi] += u[i]
-    b1ϵ[k2][zi] += 1 - u[i]
-    sumlogfactyu[k2][zi] += logfactorial(y[i] - u[i])
+    a1_post[k2][zi] += y[i]
+    b1_post[k2][zi] += 1
+    alpha0_post[k2][zi] += zberpoi[i]
+    beta0_post[k2][zi] += 1 - zberpoi[i]
+    sumlogfactystar[k2][zi] += logfactorial(y[i] - zberpoi[i])
 
     # Modify cluster/group k1/zi
-    a1λ[k1][zi] -= y[i] - u[i]
-    b1λ[k1][zi] -= 1
-    a1ϵ[k1][zi] -= u[i]
-    b1ϵ[k1][zi] -= 1 - u[i]
-    sumlogfactyu[k1][zi] -= logfactorial(y[i] - u[i])
+    a1_post[k1][zi] -= y[i]
+    b1_post[k1][zi] -= 1
+    alpha0_post[k1][zi] -= zberpoi[i]
+    beta0_post[k1][zi] -= 1 - zberpoi[i]
+    sumlogfactystar[k1][zi] -= logfactorial(y[i] - zberpoi[i])
 end
 
 function logpredlik(model::BerPoiDDP, data, i::Int, k::Int)
     d = cluster_labels(model)
+    @extract model : a1_post b1_post gamma zberpoi
     @extract data : y x
-    @extract model : a0ϵ b0ϵ a1ϵ b1ϵ a0λ b0λ a1λ b1λ sumlogfactyu u gamma
-    zi = iszero(gamma[x[i]]) ? 1 : x[i]
-    log_num = log_den = 0.0
-    if d[i] == k
-        log_num = logmglik(model, j, k)
-        a1λ[k1][zi] -= y[i] - u[i]
-        b1λ[k1][zi] -= 1
-        a1ϵ[k1][zi] -= u[i]
-        b1ϵ[k1][zi] -= 1 - u[i]
-        log_den = logmglik(model, j, k)
-        a1λ[k1][zi] += y[i] - u[i]
-        b1λ[k1][zi] += 1
-        a1ϵ[k1][zi] += u[i]
-        b1ϵ[k1][zi] += 1 - u[i]
-    else
-        log_den = logmglik(model, j, k)
-        a1λ[k1][zi] += y[i] - u[i]
-        b1λ[k1][zi] += 1
-        a1ϵ[k1][zi] += u[i]
-        b1ϵ[k1][zi] += 1 - u[i]
-        log_num = logmglik(model, j, k)
-        a1λ[k1][zi] -= y[i] - u[i]
-        b1λ[k1][zi] -= 1
-        a1ϵ[k1][zi] -= u[i]
-        b1ϵ[k1][zi] -= 1 - u[i]
-    end
-    return log_num - log_den
+    j = iszero(gamma[x[i]]) ? 1 : x[i]
+    a1kj = a1_post[k][j] - (d[i] == k) * y[i]
+    b1kj = b1_post[k][j] - (d[i] == k)
+    return logpdf(NegativeBinomial(a1kj, b1kj / (b1kj + 1)), y[i] - zberpoi[i])
 end
 
-# function logpredlik(model::BerPoiDDP, train, predict, i::Int, k::Int)
-#     d = cluster_labels(model)
-#     @extract predict : y x
-#     @extract model : a0ϵ b0ϵ a1ϵ b1ϵ a0λ b0λ a1λ b1λ sumlogfactyu u gamma
-#     zi = iszero(gamma[x[i]]) ? 1 : x[i]
-#     log_num = log_den = 0.0
-#     if d[i] == k
-#         log_num = logmglik(model, j, k)
-#         a1λ[k1][zi] -= y[i] - u[i]
-#         b1λ[k1][zi] -= 1
-#         a1ϵ[k1][zi] -= u[i]
-#         b1ϵ[k1][zi] -= 1 - u[i]
-#         log_den = logmglik(model, j, k)
-#         a1λ[k1][zi] += y[i] - u[i]
-#         b1λ[k1][zi] += 1
-#         a1ϵ[k1][zi] += u[i]
-#         b1ϵ[k1][zi] += 1 - u[i]
-#     else
-#         log_den = logmglik(model, j, k)
-#         a1λ[k1][zi] += y[i] - u[i]
-#         b1λ[k1][zi] += 1
-#         a1ϵ[k1][zi] += u[i]
-#         b1ϵ[k1][zi] += 1 - u[i]
-#         log_num = logmglik(model, j, k)
-#         a1λ[k1][zi] -= y[i] - u[i]
-#         b1λ[k1][zi] -= 1
-#         a1ϵ[k1][zi] -= u[i]
-#         b1ϵ[k1][zi] -= 1 - u[i]
-#     end
-#     return log_num - log_den
-# end
+function logpredlik(model::BerPoiDDP, train, predict, i::Int, k::Int)
+    @extract model : a1_post b1_post gamma alphaberpoi
+    @extract predict : y x
+    j = iszero(gamma[x[i]]) ? 1 : x[i]
+    a1kj = a1_post[k][j]
+    b1kj = b1_post[k][j]
+    # Compute P(znew = 1 | y, ...)
+    znew = rand(Bernoulli(alphaberpoi[k][j]))
+    return logpdf(NegativeBinomial(a1kj, b1kj / (b1kj + 1)), y[i] - znew)
+    # TODO: marginalize zberpoinew?
+    # dist = NegativeBinomial(a1kj, b1kj / (b1kj + 1))
+    # return log(
+    #     (0 + alphaberpoi[k][j]) * pdf(dist, y[i] - 1) +
+    #     (1 - alphaberpoi[k][j]) * pdf(dist, y[i])
+    # )
+end
 
 function logmglik(model::BerPoiDDP, j::Int, k::Int)
-    @extract model : a0ϵ b0ϵ a1ϵ b1ϵ a0λ b0λ a1λ b1λ sumlogfactyu u gamma
+    @extract model : a1 b1 a1_post b1_post sumlogfactystar
     return (
-        + logbeta(a1ϵ[k][j], b1ϵ[k][j])
-        - logbeta(a0ϵ, b0ϵ)
-        - a1λ[k][j] * log(b1λ[k][j])
-        + a0λ * log(b0λ)
-        + loggamma(a1λ[k][j])
-        - loggamma(a0λ)
-        - sumlogfactyu[k][j]
+        a1 * log(b1) - a1_post[k][j] * log(b1_post[k][j]) +
+        loggamma(a1_post[k][j]) - loggamma(a1) -
+        sumlogfactystar[k][j]
     )
 end
 
@@ -219,10 +192,28 @@ function update_gamma!(rng::AbstractRNG, model::BerPoiDDP, data)
     end
 end
 
-# function update_u!()
+function update_alphaberpoi!(rng::AbstractRNG, model::BerPoiDDP, data)
+    @extract model : ngroups a1_post b1_post alphaberpoi
+    while length(a1_post) < cluster_capacity(model)
+        add_cluster!(model)
+    end
+    for g in 1:ngroups, k in active_clusters(model)
+        alphaberpoi[k][g] = rand(rng, Beta(a1_post[k][g], b1_post[k][g]))
+    end
+end
+
+function update_lambdapoi!(rng::AbstractRNG, model::BerPoiDDP, data)
+    @extract model : ngroups alpha0_post beta0_post lambdaberpoi
+    while length(alpha0_post) < cluster_capacity(model)
+        add_cluster!(model)
+    end
+    for g in 1:ngroups, k in active_clusters(model)
+        lambdaberpoi[k][g] = rand(rng, Gamma(alpha0_post[k][g], beta0_post[k][g]))
+    end
+end
 
 function update_hyperpars!(rng::AbstractRNG, model::BerPoiDDP, data)
     update_gamma!(rng, model, data)
+    update_lambdapoi!(rng, model, data)
+    update_alphaberpoi!(rng, model, data)
 end
-
-# Tõnu Kollo (tonu.kollo@ut.ee) University of Tartu, Tartu, Estonia
