bfe4c30b54f01e42d0c7b94ea26aa1a9d5e316bf
Iván Gutiérrez <ivangutierrez1988@gmail.com>
2024-03-21 23:00:33 -0300

    Add an epsilon to `f` when computing the shift function.

--- src/shift_function.jl
+++ src/shift_function.jl
@@ -5,13 +5,15 @@ function shift_function(fpost)
 
     # Create a quantile function
     v = tbl_f0.y
-    w = aweights(tbl_f0.f / sum(tbl_f0.f))
+    f = tbl_f0.f .+ sqrt.(eps())
+    w = aweights(f / sum(f))
     custom_quantile(x) = quantile(v, w, x)
 
     # Compute the shift function for each treatment group
     tbl_shift =
         tbl_f1 |>
         x -> sort(x, [:group, :y]) |>
+        x -> transform(x, :f => (x -> x .+ sqrt.(eps())) => :f) |>
         x -> groupby(x, :group) |>
         x -> transform(x, :f => (x -> cumsum(x)) => :F; ungroup = false) |>
         x -> transform(x, :F => (x -> x / maximum(x)) => :F; ungroup = false) |>
